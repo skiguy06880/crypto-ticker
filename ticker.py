7bf1fd7f49055c2ef022e821fc51957de2f3fed6
skiguy06880 <90738873+skiguy06880@users.noreply.github.com>
2021-09-18 00:57:11 -0400

    Update ticker.py

--- ticker.py
+++ ticker.py
@@ -117,7 +117,7 @@ class Ticker(Frame):
         graphics.DrawText(
             canvas, font_change, change_x, 10, change_color, asset['change_24h']
         )
-        graphics.DrawLine(canvas, 30, 50, 0, 50, price_color)
+        graphics.DrawLine(canvas, 30, 50, 0, 50, main_color)
 
         return canvas
 
