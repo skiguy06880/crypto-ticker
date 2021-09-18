f50b35ab79c915fd626e259c6991f90d38865943
skiguy06880 <90738873+skiguy06880@users.noreply.github.com>
2021-09-17 22:13:58 -0400

    update ticker.py
    Capital Symbol

--- ticker.py
+++ ticker.py
@@ -83,6 +83,7 @@ class Ticker(Frame):
         # Create fonts for displaying prices
         font_symbol = graphics.Font()
         font_symbol.LoadFont('fonts/7x13.bdf')
+        font_symbol.upper()
 
         font_price = graphics.Font()
         font_price.LoadFont('fonts/6x12.bdf')
@@ -109,7 +110,7 @@ class Ticker(Frame):
             font_price.LoadFont('fonts/5x8.bdf')
 
         # Draw the elements on the canvas
-        graphics.DrawText(canvas, font_symbol, 3, 12, main_color, upper(asset['symbol']))
+        graphics.DrawText(canvas, font_symbol, 3, 12, main_color, asset['symbol'])
         graphics.DrawText(canvas, font_price, 3, 28, main_color, asset['price'])
         graphics.DrawText(
             canvas, font_change, change_x, 10, change_color, asset['change_24h']
